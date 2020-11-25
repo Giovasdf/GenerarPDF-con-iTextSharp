020b71891a7309bad59793360092130b52ca4f79
Giovanni Molina <gi.molina@profesor.duoc.cl>
2020-11-25 10:40:04 -0300

    Agregar archivos de proyecto.

---
+++ GenerarPDF/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace GenerarPDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var documento1 = new Document();
+
+            string path = "PDFs";
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            PdfWriter.GetInstance(documento1, new FileStream(path + "/DocumentoPrueba123.pdf", FileMode.Create));
+
+            documento1.Open();
+            documento1.Add(new Paragraph("Este es mi pdf de arquitectura"));
+            documento1.Close();
+
+            Console.ReadLine();
+           
+        }
+    }
+}
